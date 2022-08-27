cbbaf1e1d78dd7a898d5cbf1a76f33a6ffea49db
doppy10 <92343211+doppy10@users.noreply.github.com>
2022-08-27 01:36:34 -0400

    again

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -11,7 +11,7 @@ const Login = () => {
         <div className= "center">
             
             <img src= {cloudyLogo} className= "img"></img>
-            <a href= 'https://discord.com/api/oauth2/authorize?client_id=960271837365956638&redirect_uri=https%3A%2F%2F6309a83a6be883350f605e2a--merry-beijinho-86d210.netlify.app%2Fsubscriptions&response_type=code&scope=identify%20email'>
+            <a href= 'https://discord.com/api/oauth2/authorize?client_id=960271837365956638&redirect_uri=https%3A%2F%2F6309ab65371d540008b1781b--merry-beijinho-86d210.netlify.app%2Fsubscriptions&response_type=code&scope=identify%20email'>
               <button className= "button">
                 Login
               </button>
